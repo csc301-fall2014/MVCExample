031b68b3e494853ed7efd3221f71d38215f56591
Joey Freund <joey@cs.toronto.edu>
2014-11-06 18:13:47 -0500

    View only

--- src/csc301/mvcExample/Main.java
+++ src/csc301/mvcExample/Main.java
@@ -1,17 +1,40 @@
 package csc301.mvcExample;
 
-import csc301.mvcExample.service.SimpleStockMarket;
+import java.math.BigDecimal;
+
 import csc301.mvcExample.view.GUI;
+import csc301.mvcExample.view.GUIEventListener;
 
 public class Main {
+	
+	public static class DummyEventHandler implements GUIEventListener {
+	
+		@Override
+		public void onPauseClicked() {
+			System.out.println("Pause clicked");
+		}
 
-	public static void main(String[] args) {
-		SimpleStockMarket stockMarket = new SimpleStockMarket();
-		GUI gui = new GUI();
-		
-		Controller controller = new Controller(stockMarket, gui, stockMarket);
-		gui.addEventListener(controller);
-		gui.setVisible(true);
-		controller.start();
+		@Override
+		public void onResumeClicked() {
+			System.out.println("Resume clicked");
+		}
 	}
+	
+	
+	
+
+	public static void main(String[] args) throws InterruptedException {
+		GUI gui = new GUI();
+		gui.addEventListener(new DummyEventHandler());
+		gui.setVisible(true);
+		
+		BigDecimal price = new BigDecimal("296.65");
+		for (int i = 0; i < 5; i++) {
+			price = price.add(new BigDecimal(i));
+			gui.onStockUpdate("AMZN", price, true, new BigDecimal("1.2"));
+			Thread.sleep(1000);
+		}
+	}
+
+	
 }
